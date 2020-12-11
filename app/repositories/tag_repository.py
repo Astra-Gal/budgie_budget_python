aa23c98c82bfb5c17330300e4e8129e7d5c61d0b
Astra-Gal <giulia.galastro@gmail.com>
2020-12-11 20:39:27 +0000

    added functions to tag repository, and checked that I could add tag

---
+++ app/repositories/tag_repository.py
@@ -0,0 +1,54 @@
+from app.db.run_sql import run_sql
+
+from app.models.tag import Tag
+# from app.models.transaction import Transaction
+# from app.models.merchant import Merchant
+
+# save
+def save(tag):
+    sql = "INSERT INTO tags (category) VALUES (%s) RETURNING id"
+    values = [tag.category]
+    results = run_sql(sql, values)
+    tag.id = results[0]['id']
+    return tag
+
+# edit 
+def update(tag):
+    sql = "UPDATE tags SET (category) = (%s) WHERE id = %s"
+    values = [tag.category, tag.id]
+    run_sql(sql, values)
+
+# select_all
+def select_all():
+    tags = []
+
+    sql = "SELECT * FROM tags"
+    results = run_sql(sql)
+    for row in results:
+        tag = Tag(row['category'], row['id'])
+        tags.append(tag)
+    return tags 
+
+
+# select(id)
+def select(id):
+    tag = None
+    sql = "SELECT * FROM tags WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        tag = Tag(result['category'], result['id'])
+    return tag
+
+
+# delete_all
+def delete_all():
+    sql = "DELETE FROM tags"
+    run_sql(sql)
+
+# delete(id)
+def delete(id):
+    sql = "DELETE FROM tags WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
--- console.py
+++ console.py
@@ -1,7 +1,16 @@
 import pdb
 from app.models.merchant import Merchant 
+from app.models.tag import Tag
 
 import app.repositories.merchant_repository as merchant_repository
+import app.repositories.tag_repository as tag_repository
+
+
+merchant_repository.delete_all()
+tag_repository.delete_all()
 
 merchant_1 = Merchant("Sainsbury")
 merchant_repository.save(merchant_1)
+
+tag_1 = Tag("Groceries")
+tag_repository.save(tag_1)
