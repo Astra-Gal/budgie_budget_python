fc272a3bd809accd8f059c4a1d13686b8d83bde3
Astra-Gal <giulia.galastro@gmail.com>
2020-12-11 14:50:45 +0000

    created tables in budgie_budget.sql

---
+++ app/db/budgie_budget.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255)
+);
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount INT,
+    merchant_id INT REFERENCES merchants(id),
+    tag_id INT REFERENCE tags(id)
+);
