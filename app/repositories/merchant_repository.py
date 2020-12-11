c4ad5c1198afd3e0a5dc3da64fc3e41fad698fb7
Astra-Gal <giulia.galastro@gmail.com>
2020-12-11 20:19:34 +0000

    added functions to merchant_repository

--- app/repositories/merchant_repository.py
+++ app/repositories/merchant_repository.py
@@ -5,7 +5,7 @@ from app.models.tag import Tag
 from app.models.transaction import Transaction
 
 
-# save
+# save a merchant
 def save(merchant):
     sql = "INSERT INTO merchants (name) VALUES (%s) RETURNING id"
     values = [merchant.name]
@@ -13,13 +13,43 @@ def save(merchant):
     merchant.id = results[0]['id']
     return merchant
 
+# edit a merchant
+def update(merchant):
+    sql = "UPDATE merchants SET (name) = (%s) WHERE id = %s"
+    values = [merchant.name, merchant.id]
+    run_sql(sql, values)
+
 
 # select_all
+def select_all():
+    merchants = []
+
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+    for row in results:
+        merchant = Merchant(row['name'], row['id'])
+        merchants.append(merchant)
+    return merchants
 
 # select(id)
+def select(id):
+    merchant = None
+    sql = "SELECT * FROM merchants WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
 
-# edit merchant - UPDATE merchants SET name = (?? %s ??) WHERE name = (?? %s ??) 
+    if result is not None:
+        merchant = Merchant(result['name'], result['id'])
+    return merchant
+
 
 # delete_all
+def delete_all():
+    sql = "DELETE FROM merchants"
+    run_sql(sql)
 
 # delete(id)
+def delete(id):
+    sql = "DELETE FROM merchants WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
