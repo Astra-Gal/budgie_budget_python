e17add8f4d8c206e20f8d14108cf0b1df2628115
Astra-Gal <giulia.galastro@gmail.com>
2020-12-11 21:33:06 +0000

    added select_all function to transaction_repository

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -3,8 +3,8 @@ from app.db.run_sql import run_sql
 from app.models.transaction import Transaction
 from app.models.merchant import Merchant
 from app.models.tag import Tag
-import app.repositories.merchant_repository
-import app.repositories.tag_repository
+import app.repositories.merchant_repository as merchant_repository
+import app.repositories.tag_repository as tag_repository
 
 
 
@@ -17,6 +17,18 @@ def save(transaction):
     return transaction
 
 # select_all
+def select_all():
+    transactions = []
+
+    sql = "SELECT * FROM transactions"
+    results = run_sql(sql)
+
+    for row in results:
+        merchant = merchant_repository.select(row['merchant_id'])
+        tag = tag_repository.select(row['tag_id'])
+        transaction = Transaction(row['amount'], merchant, tag, row['id'])
+        transactions.append(transaction)
+    return transactions
 
 # select(id)
 
