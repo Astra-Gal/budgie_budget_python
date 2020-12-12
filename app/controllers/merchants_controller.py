d875749a9d82b5c87df43edd0e798bffb7bd64b4
Astra-Gal <giulia.galastro@gmail.com>
2020-12-12 17:25:35 +0000

    started Flask routes and html templates

--- app/controllers/merchants_controller.py
+++ app/controllers/merchants_controller.py
@@ -1,2 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from app.models.merchant import Merchant
+import app.repositories.merchant_repository as merchant_repository
+
+merchants_blueprint = Blueprint("merchants", __name__)
+
+@merchants_blueprint.route("/merchants")
+def merchants():
+    merchants = merchant_repository.select_all()
+    return render_template("merchants/index.html", merchants = merchants)
