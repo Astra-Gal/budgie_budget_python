cd006557bdf31907d32d488f08bd8d7e692ee7b2
Astra-Gal <giulia.galastro@gmail.com>
2020-12-13 13:16:16 +0000

    got tags index route working, apart from the mysteriously invisible jinja content

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, Blueprint, render_template, redirect, request, url_for
 
 from budgie.controllers.merchants_controller import merchants_blueprint
 from budgie.controllers.transactions_controller import transactions_blueprint
-
+from budgie.controllers.tags_controller import tags_blueprint
 
 app = Flask(__name__, template_folder='budgie/templates', static_folder='budgie/static')
 
 app.register_blueprint(merchants_blueprint)
 app.register_blueprint(transactions_blueprint)
+app.register_blueprint(tags_blueprint)
 
 # import blueprint stuff
 
---
+++ budgie/controllers/tags_controller.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+
+from budgie.models.transaction import Transaction
+from budgie.models.merchant import Merchant
+from budgie.models.tag import Tag
+
+import budgie.repositories.transaction_repository as transaction_repository
+import budgie.repositories.merchant_repository as merchant_repository
+import budgie.repositories.tag_repository as tag_repository
+
+tags_blueprint = Blueprint("tags", __name__)
+
+# show all tags
+@tags_blueprint.route("/tags")
+def tags():
+    tags = tag_repository.select_all()
+    return render_template("tags/index.html", tags=tags)
